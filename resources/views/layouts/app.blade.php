18548ca78a818525f03c8b40bb1b973ad3e0f394
kabeeenn <alfathroziq94@gmail.com>
2025-07-06 13:36:22 +0700

    WIP fix-app-blade-&-vite-config

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,8 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link rel="stylesheet" href="{{ asset('build/assets/app-CWlvJSii.css') }}">
+        <script src="{{ asset('build/assets/app-DNxiirP_.js') }}" defer></script>
 
         <!-- Styles -->
         @livewireStyles
