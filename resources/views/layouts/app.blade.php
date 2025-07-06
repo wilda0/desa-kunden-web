cd448d9366a36416b90a3aa6c71a48d2318b39a3
kabeeenn <alfathroziq94@gmail.com>
2025-07-06 14:26:08 +0700

    WIP back-code-vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,8 +14,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        <link rel="stylesheet" href="{{ asset('build/assets/app-CWlvJSii.css') }}">
-        <script src="{{ asset('build/assets/app-DNxiirP_.js') }}" defer></script>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <!-- Styles -->
         @livewireStyles
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: 'public/build',
+    base: '/public/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
